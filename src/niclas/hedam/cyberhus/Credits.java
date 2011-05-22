65fa80db248f63bd3de566405327b76964c037da
Niclas Hedam <niclas@frax.dk>
2011-05-22 13:38:30 +0200

    Credits added

---
+++ src/niclas/hedam/cyberhus/Credits.java
@@ -0,0 +1,15 @@
+package niclas.hedam.cyberhus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Credits extends Activity {
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credits);
+	}
+}
