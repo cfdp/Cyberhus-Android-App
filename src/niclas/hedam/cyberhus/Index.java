b5b7416b831e196fd9bf5d2483c839528a2eb1e4
Niclas Hedam <niclas@frax.dk>
2011-06-07 15:51:57 +0200

    Onclick + if-else created. Code has been formatted. Some image files were missing in past commits, but they are now committed too.

--- src/niclas/hedam/cyberhus/Index.java
+++ src/niclas/hedam/cyberhus/Index.java
@@ -7,7 +7,6 @@ import java.net.URLConnection;
 
 import org.apache.http.util.ByteArrayBuffer;
 
-
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -19,172 +18,190 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class Index extends Activity {
-    /** Called when the activity is first created. */
-    
-    //SET DEBUG TO TRUE, IF THE CHECKER SHOULD DOWNLOAD FROM DEBUG-SCRIPT (Frax.dk)
-    boolean Debug = true;
-    
-    //Objects that REQUIRES to be global in the class
-    public ImageView img = null;
+	/** Called when the activity is first created. */
+
+	// SET DEBUG TO TRUE, IF THE CHECKER SHOULD DOWNLOAD FROM DEBUG-SCRIPT
+	// (Frax.dk)
+	boolean Debug = true;
+
+	// Objects that REQUIRES to be global in the class
+	public ImageView img = null;
 	public TextView txt = null;
 	public Button but = null;
 	public ToggleButton tog = null;
 	public boolean crashed = false;
-    
-    @Override
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        ImageButton wb = (ImageButton)this.findViewById(R.id.wwwbutton);
-        ImageButton fb = (ImageButton)this.findViewById(R.id.fbutton);
-        ImageButton ib = (ImageButton)this.findViewById(R.id.ibutton);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    	runnable.run();
-        Button chatter = (Button)this.findViewById(R.id.button1);
-        chatter.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-        	  Intent intent = new Intent(Index.this, Chat.class);
-              startActivity(intent);
-        	  finish();
-          }
-        });
-        ImageView banner = (ImageView)findViewById(R.id.imageView1);
-        wb.setBackgroundColor(Color.TRANSPARENT);
-        fb.setBackgroundColor(Color.TRANSPARENT);
-        ib.setBackgroundColor(Color.TRANSPARENT);
-        wb.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-        	  Bundle bundle = new Bundle();
-        	  bundle.putString("url", "www");
-        	  Intent newIntent = new Intent(Index.this, URL.class);
-        	  newIntent.putExtras(bundle);
-        	  startActivityForResult(newIntent, 0);
-          }
-        });
-    }
-    private Handler mHandler = new Handler();
-    private String html = null;
-    /** Updates the label / TextView with the information from the other thread */
-    private Runnable showUpdate = new Runnable(){
-        @Override
-		public void run(){
-        	img = (ImageView)findViewById(R.id.Pointer);
-        	txt = (TextView)findViewById(R.id.Txt);
-        	but = (Button)findViewById(R.id.button1);
-    		tog = (ToggleButton)findViewById(R.id.toggleButton1);
-        	if(html.contains("green.png") == true){
-        		img.setImageResource(R.drawable.green);
-        		txt.setText("Tryk på knappen under dig for at oprette forbindelse til Cyberhus chatrådgivning.");
-        		Log.d("Cyberhus","Grøn");
-        		but.setEnabled(true);
-        		tog.setEnabled(false);
-        		if(tog.isChecked()){
-        			NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        			int icon = R.drawable.icon;
-        			CharSequence text = "Chatrådgivningen er åben";
-        			CharSequence contentTitle = "Chatrådgivningen er åben";
-        			CharSequence contentText = "Klik her for at oprette forbindelse til chatrådgivningen.";
-        			long when = System.currentTimeMillis();
-        			Intent intent = new Intent(Index.this, Chat.class);
-        			PendingIntent contentIntent = PendingIntent.getActivity(Index.this, 0, intent, 0);
-        			Notification notification = new Notification(icon,text,when);
-        			notification.defaults |= Notification.DEFAULT_VIBRATE;
-        			notification.setLatestEventInfo(Index.this, contentTitle, contentText, contentIntent);
-        			notificationManager.notify(0, notification);
-        			tog.setChecked(false);
-        			finish();
-        		}
-        	}
-        	else if(html.contains("yellow.png") == true){
-        		img.setImageResource(R.drawable.yellow);
-        		txt.setText("Der er optaget på chatrådgivningen. Skiltet opdateres hvert 20. sekund.");
-        		Log.d("Cyberhus","Gul");
-        		but.setEnabled(false);
-        		tog.setEnabled(true);
-        	}
-        	else if(html.contains("red.png") == true){
-        		img.setImageResource(R.drawable.red);
-        		txt.setText("Chatten er lukket. Den er åben mandag-torsdag klokken 14-19 og fredag 13-19.");
-        		Log.d("Cyberhus","Rød");
-        		but.setEnabled(false);
-        		tog.setEnabled(true);
-        	}else{
-        		SetError();
-        	}
-        }
-    };
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable() {
 
-    	@Override
+	private final Handler mHandler = new Handler();
+	private String html = null;
+	/** Updates the label / TextView with the information from the other thread */
+	private final Runnable showUpdate = new Runnable() {
+		@Override
 		public void run() {
-    		Thread checkUpdate = new Thread() {
-    	        @Override
+			img = (ImageView) findViewById(R.id.Pointer);
+			txt = (TextView) findViewById(R.id.Txt);
+			but = (Button) findViewById(R.id.button1);
+			tog = (ToggleButton) findViewById(R.id.toggleButton1);
+			if (html.contains("green.png") == true) {
+				img.setImageResource(R.drawable.green);
+				txt.setText("Tryk på knappen under dig for at oprette forbindelse til Cyberhus chatrådgivning.");
+				Log.d("Cyberhus", "Grøn");
+				but.setEnabled(true);
+				tog.setEnabled(false);
+				if (tog.isChecked()) {
+					final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+					final int icon = R.drawable.icon;
+					final CharSequence text = "Chatrådgivningen er åben";
+					final CharSequence contentTitle = "Chatrådgivningen er åben";
+					final CharSequence contentText = "Klik her for at oprette forbindelse til chatrådgivningen.";
+					final long when = System.currentTimeMillis();
+					final Intent intent = new Intent(Index.this, Chat.class);
+					final PendingIntent contentIntent = PendingIntent
+							.getActivity(Index.this, 0, intent, 0);
+					final Notification notification = new Notification(icon,
+							text, when);
+					notification.defaults |= Notification.DEFAULT_VIBRATE;
+					notification.setLatestEventInfo(Index.this, contentTitle,
+							contentText, contentIntent);
+					notificationManager.notify(0, notification);
+					tog.setChecked(false);
+					finish();
+				}
+			} else if (html.contains("yellow.png") == true) {
+				img.setImageResource(R.drawable.yellow);
+				txt.setText("Der er optaget på chatrådgivningen. Skiltet opdateres hvert 20. sekund.");
+				Log.d("Cyberhus", "Gul");
+				but.setEnabled(false);
+				tog.setEnabled(true);
+			} else if (html.contains("red.png") == true) {
+				img.setImageResource(R.drawable.red);
+				txt.setText("Chatten er lukket. Den er åben mandag-torsdag klokken 14-19 og fredag 13-19.");
+				Log.d("Cyberhus", "Rød");
+				but.setEnabled(false);
+				tog.setEnabled(true);
+			} else {
+				SetError();
+			}
+		}
+	};
+	private final Handler handler = new Handler();
+	private final Runnable runnable = new Runnable() {
+
+		@Override
+		public void run() {
+			final Thread checkUpdate = new Thread() {
+				@Override
 				public void run() {
-    	            try {
-    	            	URL updateURL = null;
-    	            	if(Debug == true){
-    	            		updateURL = new URL("http://frax.dk/method.php");
-    	            	}
-    	            	else
-    	            	{
-    	            		updateURL = new URL("http://chat.cyberhus.dk/lyskryds.php?action=checklys");
-    	            	}
-    	                URLConnection conn = updateURL.openConnection();
-    	                InputStream is = conn.getInputStream();
-    	                BufferedInputStream bis = new BufferedInputStream(is);
-    	                ByteArrayBuffer baf = new ByteArrayBuffer(50);
-    	 
-    	                int current = 0;
-    	                while((current = bis.read()) != -1){
-    	                    baf.append((byte)current);
-    	                }
-    	                /* Convert the Bytes read to a String. */
-    	                html = new String(baf.toByteArray());
-    	                mHandler.post(showUpdate);
-    	                this.interrupt();
-    	            } catch (Exception e) {
-    	            	html = "error";
-    	            	mHandler.post(showUpdate);
-    	            	this.interrupt();
-    	            }
-    	        }
-    	    };
-    	    
-    	    checkUpdate.start();
-    		/*
-    		 * Now register it for running next time
-    		 */
+					try {
+						URL updateURL = null;
+						if (Debug == true) {
+							updateURL = new URL("http://frax.dk/method.php");
+						} else {
+							updateURL = new URL(
+									"http://chat.cyberhus.dk/lyskryds.php?action=checklys");
+						}
+						final URLConnection conn = updateURL.openConnection();
+						final InputStream is = conn.getInputStream();
+						final BufferedInputStream bis = new BufferedInputStream(
+								is);
+						final ByteArrayBuffer baf = new ByteArrayBuffer(50);
 
-    		handler.postDelayed(this, 20000);
-    	}
-    
+						int current = 0;
+						while ((current = bis.read()) != -1) {
+							baf.append((byte) current);
+						}
+						/* Convert the Bytes read to a String. */
+						html = new String(baf.toByteArray());
+						mHandler.post(showUpdate);
+						interrupt();
+					} catch (final Exception e) {
+						html = "error";
+						mHandler.post(showUpdate);
+						interrupt();
+					}
+				}
+			};
 
-    };
-    private void SetError() {
-		img = (ImageView)findViewById(R.id.Pointer);
-    	txt = (TextView)findViewById(R.id.Txt);
-        but = (Button)findViewById(R.id.button1);
-		tog = (ToggleButton)findViewById(R.id.toggleButton1);
-    	
-    	but.setEnabled(false);
+			checkUpdate.start();
+			/*
+			 * Now register it for running next time
+			 */
+
+			handler.postDelayed(this, 20000);
+		}
+
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+		final ImageButton wb = (ImageButton) findViewById(R.id.wwwbutton);
+		final ImageButton fb = (ImageButton) findViewById(R.id.fbutton);
+		final ImageButton ib = (ImageButton) findViewById(R.id.ibutton);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		runnable.run();
+		final Button chatter = (Button) findViewById(R.id.button1);
+		chatter.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final Intent intent = new Intent(Index.this, Chat.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+		wb.setBackgroundColor(Color.TRANSPARENT);
+		fb.setBackgroundColor(Color.TRANSPARENT);
+		ib.setBackgroundColor(Color.TRANSPARENT);
+		wb.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final Intent newIntent = new Intent(Index.this,
+						niclas.hedam.cyberhus.URL.class);
+				newIntent.putExtra("url", "www");
+				startActivityForResult(newIntent, 2);
+			}
+		});
+		fb.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final Intent newIntent = new Intent(Index.this,
+						niclas.hedam.cyberhus.URL.class);
+				newIntent.putExtra("url", "f");
+				startActivityForResult(newIntent, 6);
+			}
+		});
+		ib.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final Intent newIntent = new Intent(Index.this,
+						niclas.hedam.cyberhus.URL.class);
+				newIntent.putExtra("url", "i");
+				startActivityForResult(newIntent, 8);
+			}
+		});
+	}
+
+	private void SetError() {
+		img = (ImageView) findViewById(R.id.Pointer);
+		txt = (TextView) findViewById(R.id.Txt);
+		but = (Button) findViewById(R.id.button1);
+		tog = (ToggleButton) findViewById(R.id.toggleButton1);
+
+		but.setEnabled(false);
 		img.setImageResource(R.drawable.red);
-		Log.d("Cyberhus","Fejl");
+		Log.d("Cyberhus", "Fejl");
 		tog.setEnabled(true);
 		txt.setText("Der opstod en fejl under indlæsningen af chatten. Tjek din internetforbindelse.");
-		
-		
+
 	}
 }
---
+++ src/niclas/hedam/cyberhus/URL.java
@@ -0,0 +1,35 @@
+package niclas.hedam.cyberhus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class URL extends Activity {
+	WebView mWebView;
+
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credits);
+		mWebView = (WebView) findViewById(R.id.webview);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebViewClient(new WebViewClient());
+		final String param1 = getIntent().getExtras().getString("url");
+		Log.d("Cyberhus", param1);
+		if (param1.contains("www")) {
+			mWebView.loadUrl("http://cyberhus.dk");
+		} else if (param1.contains("f")) {
+			mWebView.loadUrl("http://facebook.com/cyberhus");
+		} else {
+			mWebView.loadUrl("http://cyberhus.dk/smartphone");
+		}
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+}
